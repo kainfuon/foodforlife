be08f3afbbf099ef67825a05ea1a0c0aa4cf78ed
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-10-11 10:20:33 +0700

    1st

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css'
+import { assets } from '../../assets/assets.js'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <li>home</li>
+                <li>menu</li>
+                <li>mobile-app</li>
+                <li>contact us</li>
+            </ul>
+            <div className='navbar-right'>
+                <img src={assets.search_icon} alt="" />
+                <div className="Navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>sign in</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
