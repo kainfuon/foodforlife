8b45ebbef35226d08b2fad94eb5986637c3bd8c4
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-10-13 15:11:02 +0700

    3rd

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import './Home.css'
+import Header from './../../components/Header/Header';
+import ExploreMenu from './../../components/ExploreMenu/ExploreMenu';
+import { useState } from 'react';
 const Home = () => {
+    const [category, setCategory] = useState("All")
     return (
-        <div>Home</div>
+        <div>
+            <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
+        </div>
     )
 }
 
