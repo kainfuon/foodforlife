2f06b0dfbb7fa822b1b9dd40e0626bcf6275f2c6
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-10-16 08:34:48 +0700

    Add FoodDisplay, FoodItem components, StoreContext context with JSX and CSS files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import Cart from "./pages/Cart/Cart";
-import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Navbar from './components/Navbar/Navbar';
+import StoreContextProvider from './context/StoreContext'; // Import Provider
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/orders" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <StoreContextProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+    </StoreContextProvider>
   );
 };
 
---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
+import './FoodDisplay.css';
+
+const FoodDisplay = ({ category }) => {
+    const { food_list } = useContext(StoreContext);
+
+    console.log("food_list:", food_list);
+    console.log("Selected category:", category);
+
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near you</h2>
+            <div className="food-display-list">
+                {food_list.map((item, index) => {
+                    if (category === "All" || category === item.category) {
+                        return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    }
+                })}
+            </div>
+        </div>
+    )
+}
+
+FoodDisplay.propTypes = {
+    category: PropTypes.string.isRequired,
+};
+
+export default FoodDisplay;
