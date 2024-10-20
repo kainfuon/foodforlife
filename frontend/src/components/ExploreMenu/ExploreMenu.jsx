8f1e5f5f35bc010efba59de31ea9d8630ae35f1a
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-10-20 22:03:29 +0700

    Add media queries for all components styles

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,7 +7,7 @@ const ExploreMenu = ({ category, setCategory }) => {
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
-            <p className='explore-menu-text'>Choose frome menu list to join our meal</p>
+            <p className='explore-menu-text'>Choose from menu list to join our meal</p>
             <div className="explore-menu-list">
                 {menu_list.map((item, index) => {
                     return (
