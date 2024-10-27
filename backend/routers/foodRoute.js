4104e13e3ee0969501d03aea3144c29d9d5de6dc
kainfuon <phuongtkp7@gmail.com>
2024-10-28 00:11:50 +0700

    add, get, delete food

--- backend/routers/foodRoute.js
+++ backend/routers/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addFood } from "../controllers/foodController.js"
+import { addFood, listFood, removeFood} from "../controllers/foodController.js"
 import multer from "multer"
 
 const foodRouter = express.Router();
@@ -7,7 +7,7 @@ const foodRouter = express.Router();
 // Image storage engine
 const storage = multer.diskStorage({
     destination: "uploads",
-    filename: (req,file,cb) => {
+    filename: (req, file, cb) => {
         return cb(null,`${Date.now()}${file.originalname}`)
     }
 })
@@ -15,5 +15,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage})
 
 foodRouter.post("/add", upload.single("image"),addFood)
+foodRouter.get("/list", listFood)
+foodRouter.post("/remove", removeFood)
 
 export default foodRouter;
