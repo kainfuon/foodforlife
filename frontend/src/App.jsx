6a1cc7fe9f340f505be9b4aa1b9f9204f850397d
kainfuon <phuongtkp7@gmail.com>
2024-11-10 18:49:16 +0700

    FE create user order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 import { useState } from 'react';
 import Verify from './pages/Verify/Verify';
+import MyOrders from './pages/MyOrders/MyOrders';
 
 const App = () => {
 
@@ -23,7 +24,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/orders" element={<PlaceOrder />} />
-          <Route path='verify' element={<Verify/>}/>
+          <Route path='/verify' element={<Verify/>}/>
+          <Route path='/myorders' element={<MyOrders/>}/>
         </Routes>
       </div>
       <Footer></Footer>
