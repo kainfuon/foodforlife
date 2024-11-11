ffbc3db37f95f4c73a708c54b624b28250e5b636
kainfuon <phuongtkp7@gmail.com>
2024-11-11 10:05:17 +0700

    clean github tree

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-//const MONGO_URL = 'mongodb+srv://kainfuon:Test1234567@cluster0.ayrph.mongodb.net/food-del'
+//const MONGO_URL = 'mongodb+srv://kainfuon:Test12345678@cluster0.ayrph.mongodb.net/food-del'
 
 export const connectDB = async () => {
     await mongoose.connect('mongodb://localhost:27017/my_database')
